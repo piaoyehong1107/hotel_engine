25253bb4591721aa56ccd0b207d37528a34d6a6a
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2021-06-28 21:40:34 -0500

    connected with music api

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,23 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 class SearchesController < ApplicationController
+    def index
+        url = URI("https://shazam.p.rapidapi.com/search?term=#{params['term']}")
+            http = Net::HTTP.new(url.host, url.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+            request = Net::HTTP::Get.new(url)
+            request["x-rapidapi-key"] = '6d4d8a6cd6mshe71720ae222ee71p14c900jsn605699b2c370'
+            request["x-rapidapi-host"] = 'shazam.p.rapidapi.com'
+    
+            response = http.request(request)
+            puts response.read_body
+
+            render json: response.read_body
+
+    end
+
 end
